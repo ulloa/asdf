b25f5e112f3bd1ae53c6beb2d4e005b3371bb47d
ulloa <samuel.ulloa@gmail.com>
2014-01-05 22:07:38 -0800

    added web.js

---
+++ web.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+var fs = require('fs');
+var htmlfile = "index.html";
+
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+//  response.send(fs.readFileSync('index.html').tostring());
+});
+
+app.use(express.static(__dirname + '/public')); // just added for static files
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
