54a5e79fe2fb3013dbb549824aa6e8f4e04dcd5d
--help <samuel.ulloa@gmail.com>
2014-01-11 15:24:52 -0800

    mongoHQ connected and some angular added

---
+++ main.js
@@ -0,0 +1,3 @@
+function FirstCtrl($scope) {
+
+}
--- web.js
+++ web.js
@@ -1,6 +1,16 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
+
+mongoose.connect(process.env.MONGOHQ_URL);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+	console.log('Connected to MongoDB');
+});
+
 app.use(express.logger());
+
 
 var fs = require('fs');
 var htmlfile = "index.html";
@@ -11,8 +21,17 @@ app.get('/', function(request, response) {
 //  response.send(fs.readFileSync('index.html').tostring());
 });
 
+var AnimalSchema = new Object({
+	name: String,
+	type: String
+});
 
-var port = process.env.PORT || 8888;
+var Animal = mongoose.model('Animal', AnimalSchema);
+var cat = new Animal({name:'Pumpkin', type:'cat'});
+cat.save()
+
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
