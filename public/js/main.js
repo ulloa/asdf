d693704ae03ad56acc002964d11f0f2b7c6141fc
ulloa <samuel.ulloa@gmail.com>
2014-02-04 18:48:41 -0800

    working picture upload and mongodb connection

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,19 @@
-app.controller('FirstCtrl', function ($scope) {
-    $scope.uploadComplete = function (content) {
-	$scope.user = JSON.parse(content); // Presumed content is a json string!
+var myapp = angular.module('app', []);
+
+myapp.controller('FirstCtrl', ['$scope', function ($scope) {
+//    $scope.uploadComplete = function (content) {
+//	$scope.user = JSON.parse(content); // Presumed content is a json string!
 	$scope.user.style = {
 	    color: $scope.user.color,
 	    "font-weight": "bold"
 	};
+	$scope.user = 'samuel';
 
 	$scope.adname = '';
 	$scope.adname.tags = '';
 	$scope.adname.gpsloc = '';
 	
-    };
+//    };
 
     $scope.submit = function () {
 	var $scope.adobject = new Object({
--- web.js
+++ web.js
@@ -18,15 +18,55 @@ app.use(express.bodyParser());
 var fs = require('fs');
 var htmlfile = "index.html";
 
+
 app.post('/upload-full-form',function(req,res){
-    console.log(req.files);
+    console.log(req.body);
     console.log('FIRST TEST: ' + JSON.stringify(req.files));
     console.log('SECOND TEST: ' + req.files.theFile.name);
     fs.readFile(req.files.theFile.path, function (err, data) {
-        var newPath = __dirname + "/public/" + req.files.theFile.name; //put path here
-        fs.writeFile(newPath, data, function (err) {
-          res.send("hi");  
-        });
+	var AWS = require('aws-sdk');
+	AWS.config.region = 'us-west-2';
+	var uuid = require('node-uuid');
+
+	var s3 = new AWS.S3();
+
+	var bucketName = 'adlist';
+	var keyName = req.files.theFile.name;
+
+	var bodyStream = fs.createReadStream(req.files.theFile.path);
+	var params = {Bucket: bucketName, Key: keyName, Body: bodyStream};
+	s3.putObject(params, function(err, data) {
+	    if (err)
+		console.log(err)
+	    else 
+	    {
+		console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+	        var UserSchema = new Object({
+		    name: String,
+		    ads: Array
+		});
+	    
+		var adSchema = new Object({
+		    adname: req.body['user.ads.adname'],
+		    tags: req.body['user.ads.tags'],
+		    imagepath: bucketName + '/' + keyName,
+		    gpsloc: {
+			lat: req.body['user.ads.gpsloc'],
+			lon: Number
+		    }
+		});
+
+	        var Account = mongoose.model('Accounts', UserSchema);
+ 	        var newuser = new Account({name: req.body['user.name'], ads: [adSchema]});
+                newuser.save()
+	        res.send("Successfully uploaded to the S3 Storage");
+	    }
+	});
+
+//        var newPath = __dirname + "/public/" + req.files.theFile.name; //put path here
+//        fs.writeFile(newPath, data, function (err) {
+//          res.send("hi");  
+//        });
     });
 });
 
@@ -35,15 +75,6 @@ app.get('/', function(request, response) {
     response.send(html);
 //  response.send(fs.readFileSync('index.html').tostring());
 });
-
-var AnimalSchema = new Object({
-	name: String,
-	type: String
-});
-
-var Animal = mongoose.model('Animal', AnimalSchema);
-var cat = new Animal({name:'Pumpkin', type:'cat'});
-cat.save()
 
 
 var port = process.env.PORT || 8080;
